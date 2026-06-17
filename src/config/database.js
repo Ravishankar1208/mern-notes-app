bf02cdc79b009646fc98a7f4b1ddb487052b19eb
Ravishankar1208 <tiwariravishankar1208@gmail.com>
2026-06-17 19:30:32 +0530

    check mongodb connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
-async function connectToDB(){
-  await mongoose.connect(process.env.MONGO_URI);
 
-  console.log("connected to database")
+async function connectToDB(){
+
+ await mongoose.connect(process.env.MONGO_URI)
+
+
+ console.log("connected to database")
+
+ console.log("HOST:", mongoose.connection.host)
+
+ console.log("DATABASE:", mongoose.connection.name)
+
 }
+
 
 module.exports = connectToDB
